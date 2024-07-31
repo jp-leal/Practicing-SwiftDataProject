54d1d4d544d4d2b3e663eed4ba2bd9b33b4c99d2
joaobug <joaoenfrance@gmail.com>
2024-07-31 13:27:15 -0300

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Joao Leal on 31/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
